85e3ff487b761c1b9dc87ea077c6f2e00b98bfc5
Ravi <sangana575@gmail.com>
2020-04-15 19:04:47 +0530

    added a column quantity in discount table

---
+++ pubs/Migrations/1.1.0-Changes/002_20200415-1903_11201441.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="1b993cec-432f-4475-a400-b58780291479" />
+GO
+
+PRINT N'Altering [dbo].[discounts]'
+GO
+ALTER TABLE [dbo].[discounts] ADD
+[quantity] [nchar] (10) NULL
+GO
--- pubs/Schema-Model/dbo/Tables/discounts.sql
+++ pubs/Schema-Model/dbo/Tables/discounts.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[discounts]
 [stor_id] [char] (4) NULL,
 [lowqty] [smallint] NULL,
 [highqty] [smallint] NULL,
-[discount] [decimal] (4, 2) NOT NULL
+[discount] [decimal] (4, 2) NOT NULL,
+[quantity] [nchar] (10) NULL
 )
 GO
 ALTER TABLE [dbo].[discounts] ADD CONSTRAINT [FK__discounts__stor___4F7CD00D] FOREIGN KEY ([stor_id]) REFERENCES [dbo].[stores] ([stor_id])
